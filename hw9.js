e95356d889427199ebf466ea9ab2df58c594b7d2
Anastasiia Holub <67327832+NancyG5@users.noreply.github.com>
2021-08-04 11:25:31 +0300

    Update hw9.js

--- hw9.js
+++ hw9.js
@@ -1,5 +1,4 @@
 function myBody(e) {
-    e.preventDefault();
     let x = e.clientX;
     let y = e.clientY;
     let coor = "Координаты: X: " + x + "; Y: "+ y;
@@ -7,8 +6,6 @@ function myBody(e) {
 }
 
 function myFunc(e) {
-    e.preventDefault();
-    e.stopPropagation();
     let x = e.offsetX;
     let y = e.offsetY;
     let coor1 = "Координаты: X : " + x + "; Y : " + y;
